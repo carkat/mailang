bf14b04a46a908d248911b99e90e8ca8f33a2013
nanthil <macks.smart@hotmail.com>
2018-04-23 19:22:17 -0600

    better logic for funcs vs vars

--- parser2.js
+++ parser2.js
@@ -1,26 +1,52 @@
 const fs = require('fs')
 
 const isAssign = ln => ln.includes('<-')
-const isFNDecl = ln => ln[0] !== '|' && (ln[ln.length - 1] === '=' || ln.includes(' = '))
-
-const getUntilNextLineIsAssignmentOrDecl = (ln, lineNum, lns) =>{
+const isFNDecl = ln => ln[0] !== '|' 
+  && ln.split('=')[0].indexOf('<') === -1
+  && (
+    ln[ln.length - 1] === '=' 
+    || ln.includes(' = ')
+  ) 
+const isWhere  = ln => ln[0] === '?'
+const getUntilNextLineIsBreak = (ln, lineNum, lns) =>{
   let result = ln
   let shouldContinue = true
   for(let i = lineNum; i < lns.length -1; ++i){
     const nextLine = lns[++lineNum]
-    if(isAssign(nextLine))      return result
+    if(!nextLine.length)        return result
+    else if(isFNDecl(nextLine)) return result
+    else result = result +  ' \n ' + nextLine
+    lns[lineNum] = ''
+  }
+  return result
+}
+
+const getUntilNextLineIsAssignmentOrDecl = (ln, lineNum, lns) =>{
+  let result = ln
+  let shouldContinue = true
+  const linesToClear = []
+  for(let i = lineNum; i < lns.length -1; ++i){
+    const nextLine = lns[++lineNum]
+    linesToClear.push(lineNum)
+    if(isAssign(nextLine)){
+      return result
+    }
     else if(!nextLine.length)   return result
     else if(isFNDecl(nextLine)) return result
     else result = result +  ' \n ' + nextLine
   }
+  lns.forEach((x,i) => linesToClear)
+
+  
   return result
 }
+
 const groupAssignsAndDecls = group => 
   group
     .map((ln, lineNum, lns) =>
-      isAssign(ln)
-        ? getUntilNextLineIsAssignmentOrDecl(ln, lineNum, lns)
-      : isFNDecl(ln)
+       isFNDecl(ln)
+        ? getUntilNextLineIsBreak(ln, lineNum, lns)
+      : isAssign(ln)
         ? getUntilNextLineIsAssignmentOrDecl(ln, lineNum, lns)
       : '')
     .filter(ln => ln.length)
@@ -43,30 +69,34 @@ const parseFile = file => {
 
 const parseCode = code => {
   console.log(code.map(ln => {
-    if(isAssign(ln)){
+    if(isFNDecl(ln)){
+      const functionBody = ln
+        .split(' = ').slice(1).join(' = ').split('\n').filter(x=>x).map(x => x.trim())
+      const functionHeader = ln
+        .split(' = ')[0]
+
+      return {
+        type: 'function',
+        name: functionHeader.split(' ')[0],
+        args: functionHeader.split(' ').slice(1),
+        body: functionBody
+      }
+    }
+    else if(isAssign(ln)){
       const splitLine = ln
         .split('<-')
         .map(ln => ln.trim())
       return {
+        type: 'variable',
         name: splitLine[0],
-        value: splitLine[1].split('\n')
-      }
-    }
-    else if(isFNDecl(ln)){
-      const functionBody = ln
-        .split(' = ').slice(1).join('=').split('\n').filter(x=>x)
-      const functionName = ln
-        .split(' = ')[0]
-
-      return {
-        name: functionName,
-        body: functionBody
+        value: splitLine[1]
       }
     }
   }))
 
 }
 
-const file = fs.readFileSync('./test.js', 'UTF-8')
+const file = fs.readFileSync('./test.m', 'UTF-8')
 parseCode(parseFile(file))
+// console.log(parseFile(file))
 
