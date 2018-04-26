b26f60cbed38174a8864b5004edb540e653b8064
nanthil <macks.smart@hotmail.com>
2018-04-25 22:49:47 -0600

    added algos

--- newCode.js
+++ newCode.js
@@ -8,3 +8,10 @@ const rotate = (num, arr,n=num%arr.length) =>
         ? a[j] 
         : a[0]
   })
+const add = (a, b) => a + b
+const exists    = (val, arr) => arr.includes(val)
+const locate    = (val, arr) => arr.findIndex(x => x === val)
+const locateAll = (val, arr) => arr.map((x,i) => x === val && i).filter(x=>x)
+const forAll    = (fn,  arr) => arr.every((x,i,a)=>i+1<a.length?fn(x,i,a):true)
+const Sigma     = (arr, fn=add)  => arr.reduce(fn)
+
